02e5643ef8bf7cec0b078a055f24798c2ae6107e
Chinni Krishna Murthy Vadlapudi <KrishnaMurthy.Vadlapudi@us.metrixlab.com>
2021-05-08 20:10:38 +0530

    updated guard module

--- FirstProject/src/app/app.module.ts
+++ FirstProject/src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 //import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListcomponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
 import { ProductModule } from './products/product.module';
 // import { WelcomeComponent } from './home/welcome.component';
 
@@ -17,23 +11,15 @@ import { ProductModule } from './products/product.module';
   declarations: [
     AppComponent,
     // WelcomeComponent,
-    ProductListcomponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
   //  AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path:'products', component: ProductListcomponent}
-      ,{path: 'products/:id', 
-        canActivate: [ProductDetailGuard],
-      component: ProductDetailComponent}
+      
       // ,{path:'welcom' Component: WelcomeComponent}
-      ,{path: '', redirectTo: 'welcome', pathMatch: 'full'}
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'}
       ,{path:'**', redirectTo: 'welcome', pathMatch: 'full'}
 
     ]),
--- FirstProject/src/app/products/product.module.ts
+++ FirstProject/src/app/products/product.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListcomponent } from './product-list.component';
+import { ProductDetailGuard } from "./ProductDetailGuard";
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListcomponent,
+     ConvertToSpacesPipe,
+    StarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      {path:'products', component: ProductListcomponent}
+      ,{path: 'products/:id', 
+        canActivate: [ProductDetailGuard],
+      component: ProductListcomponent}
+    ])
   ]
 })
 export class ProductModule { }
